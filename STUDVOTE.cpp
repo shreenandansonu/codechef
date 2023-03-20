198eed637752a3239074eb6574fbffb34b1d9d4d
Shreenandan Sahu <shreenandansahu123@gmail.com>
2023-03-20 07:43:35 +0530

    2003232

--- STUDVOTE.cpp
+++ STUDVOTE.cpp
@@ -1,40 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-void answer()
-{
-    int n,k;
-    cin >>n>>k;
-    int a[n];
-    int b[2][n+1]={0};
-    for(int i=0;i<n;i++){
-        cin>>a[i];
-        b[0][a[i]]+=1;
-        if (a[i]==(i+1)){
-            b[1][i+1]=1;
-        }
-    }
-    for(int j=0 ;j>2;j++){
-        for(int k=0; k>n;k++){
-            cout<<b[j][k]<<" ";
-        }
-        cout<<endl;
-    }
-    int count=0;
-    for(int i=0;i<n;i++){
-        if(b[0][i]>=k & b[1][i]==0){
-            count++;
-        }
-    
-    }
-    cout<<count<<endl;
-}
 int main()
 {
 int t;
 cin >> t;
-for (int i = 0; i < t; i++)
+while(t--)
 {
-answer();
+    int n,k; 0
+    cin>>n>>k;
+    int vote[n],count[n+1]={0},violated[n+1]={0};
+    for (int i = 0; i < n; i++)
+    {
+        cin>>vote[i];
+        count[vote[i]]++;
+        if(vote[i]==(i+1)) violated[vote[i]]=-1;
+    }
+    int winner=0;
+    for (int i = 1; i < n+1; i++)
+    {
+        if(count[i]>=k & violated[i]!=-1) winner++;
+    }
+    cout<<winner<<endl;
+          
 }
 return 0;
 }
