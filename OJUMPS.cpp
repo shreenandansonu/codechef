0a7afed6c84cd58d3ef45db68ce25cf462bfa28c
Shreenandan Sahu <shreenandansahu123@gmail.com>
2023-03-21 23:45:54 +0530

    ggg

--- OJUMPS.cpp
+++ OJUMPS.cpp
@@ -1,25 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-/*string solve(){
-    int a;
-    cin>>a;
-    int x=a%6;
-    if( x==1 || x==3 || x==0 ){
-        return "yes";
-    }       
-    return "no";
-}*/
 int main()
 {
-    int a;
-    cin>>a;
-    if(a%6==0||a%6==1||a%6==3)
-    {
-        cout<<"yes"<<endl;
-    }
-    else
-    {
-        cout<<"no"<<endl;
-    }
+int a;
+cin>>a;
+int x=a%6;
+if(x==1 || x==3 || x==0){
+    cout<<"yes"<<endl;
+}
+else{
+    cout<<"no"<<endl;
+}
 return 0;
 }
