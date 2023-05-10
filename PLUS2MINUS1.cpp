b2f996748d6be3e5f56382dbb0bd0c25f7512eab
Shreenandan Sahu <shreenandansahu123@gmail.com>
2023-05-10 20:54:14 +0530

    100520231

---
+++ PLUS2MINUS1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+int t;
+cin >> t;
+while(t--)
+{
+    int y;
+    cin>>y;
+    if(y==0) cout<<1<<endl;
+    else cout<<3*y<<endl;
+}
+return 0;
+}
